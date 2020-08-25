8bffbd8010b8338aefbf6dbcaa249a8ffb9f25f2
BigManThomas <55023451+BigManThomas@users.noreply.github.com>
2020-08-25 15:41:35 +0100

    fix

---
+++ vrp_pilotJob/pilotJob_config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.startJob = {x = -983.04, y = -2947.38, z = 13.95}
+
+Config.planeSpawn = {x = -1005.99, y = -2987.48, z = 13.94, heading = 60.0}
+
+Config.deletePlane = {x = -981.02, y = -3154.1, z = 13.94}
+
+Config.privatePlane = 'LUXOR' -- spawn code of whatever private plane you want to use
+
+Config.commercialPlane = 'JET' -- spawn code of whatever commercial plane you want to use
+
+Config.pickupLocs = {
+    [1] = {x = -1325.07, y = -2708.82, z = 13.94},
+    [2] = {x = -1409.92, y = -2645.75, z = 13.94},
+    [3] = {x = -1296.5, y = -2531.59, z = 13.94}
+}
+
+Config.deliveryLocs = {
+    One = {x = -2290.9, y = 3114.89, z = 32.82},
+    Two = {x = 1629.27, y = 3231.09, z = 40.41}
+}
+
+Config.payPerPassenger = 750
