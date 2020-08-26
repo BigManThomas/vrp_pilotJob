7746b5fa36986e594999b8ed13f010098f659eb1
BigManThomas <55023451+BigManThomas@users.noreply.github.com>
2020-08-26 13:23:58 +0100

    change z coords

--- vrp_pilotJob/pilotJob_config.lua
+++ vrp_pilotJob/pilotJob_config.lua
@@ -11,14 +11,14 @@ Config.privatePlane = 'LUXOR' -- spawn code of whatever private plane you want t
 Config.commercialPlane = 'JET' -- spawn code of whatever commercial plane you want to use
 
 Config.pickupLocs = {
-    [1] = {x = -1325.07, y = -2708.82, z = 13.94},
-    [2] = {x = -1409.92, y = -2645.75, z = 13.94},
-    [3] = {x = -1296.5, y = -2531.59, z = 13.94}
+    [1] = {x = -1325.07, y = -2708.82, z = 13.78},
+    [2] = {x = -1409.92, y = -2645.75, z = 13.78},
+    [3] = {x = -1296.5, y = -2531.59, z = 13.78}
 }
 
 Config.deliveryLocs = {
-    One = {x = -2290.9, y = 3114.89, z = 32.82},
-    Two = {x = 1629.27, y = 3231.09, z = 40.41}
+    One = {x = -2290.9, y = 3114.89, z = 32.66},
+    Two = {x = 1629.27, y = 3231.09, z = 40.25}
 }
 
 Config.payPerPassenger = 750
